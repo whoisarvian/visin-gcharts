51ad3a0c692f3deccc212da18ba20888093b1157
Rivani Arvian Pratama <arviannewbie473@gmail.com>
2020-04-13 12:55:29 +0700

    visin gcharts

---
+++ application/controllers/Visin.php
@@ -0,0 +1,143 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Visin extends CI_Controller {
+
+    public function index()
+	{
+        $dataUrl=base_url('assets/sales.json');
+        $dataStringJson=file_get_contents($dataUrl);
+        $dataJson=json_decode($dataStringJson);
+        //mengambil data row
+        $data=$dataJson[2]->data;
+        //memanggil fungsi region()
+        $output['region']=$this->region($data);
+        $output['sales']=$this->sales($data);
+        $output['produk']=$this->produk($data);
+        $output['bulanan']=$this->bulanan($data);    
+        //mengirim variabel $output ke view      
+        $this->load->view('visin',$output);
+        //echo json_encode($output['bulanan']);
+    }
+
+    function region($data)
+    {
+        $result=array();
+        foreach($data as $row)
+        {
+            if(isset($result[$row->Region]) == false)
+            {
+                $result[$row->Region]=$row->Units;
+            }else{
+                $units=$result[$row->Region];
+                $result[$row->Region]=$units + $row->Units;
+            }
+        };
+        //konversi dalam format tabulasi
+        $keys=array_keys($result);
+        $tabs=[['Region','Units']];
+        foreach($keys as $row)
+        {
+            $dt=[$row,$result[$row]];
+            array_push($tabs,$dt);
+        }
+        return json_encode($tabs);
+    }
+
+    function sales($data)
+    {
+        $result=array();
+        foreach($data as $row)
+        {
+            if(isset($result[$row->Rep]) == false)
+            {
+                $result[$row->Rep]=$row->Units;
+            }else{
+                $units=$result[$row->Rep];
+                $result[$row->Rep]=$units + $row->Units;
+            }
+        };
+        //sorting data berdasarkan value array secara menurun
+        arsort($result);
+        //konversi dalam format tabulasi
+        $keys=array_keys($result);
+        $tabs=[['Sales','Units']];
+        foreach($keys as $row)
+        {
+            $dt=[$row,$result[$row]];
+            array_push($tabs,$dt);
+        }
+        return json_encode($tabs);
+    }
+
+    function produk($data)
+    {
+        $result=array();
+        foreach($data as $row)
+        {
+            if(isset($result[$row->Item]) == false)
+            {
+                $result[$row->Item]=$row->Units;
+            }else{
+                $units=$result[$row->Item];
+                $result[$row->Item]=$units + $row->Units;
+            }
+        };
+        //sorting data berdasarkan value array secara menurun
+        arsort($result);
+        //konversi dalam format tabulasi
+        $keys=array_keys($result);
+        $tabs=[['Produk','Units']];
+        foreach($keys as $row)
+        {
+            $dt=[$row,$result[$row]];
+            array_push($tabs,$dt);
+        }
+        return json_encode($tabs);
+    }
+
+    function bulanan($data)
+    {
+        $result=array();
+        foreach($data as $row)
+        {
+                //mengambil data tanggal
+            $time=strtotime($row->OrderDate);
+            $bulan=date('n',$time);
+            $tahun=date('Y',$time);		
+            if(isset($result[$tahun]) == false)
+            {
+                $result[$tahun][$bulan]=$row->Units;                
+            }else{
+                if(isset($result[$tahun][$bulan]) == false)
+                {
+                    $result[$tahun][$bulan]=(int)$row->Units;
+                }else{
+                    $result[$tahun][$bulan]=$result[$tahun][$bulan]+ (int) $row->Units;
+                }
+            }
+        }; 
+        //mengkonversi index data $result kedalam array  
+        $keys=array_keys($result);
+        //membuat data inisial
+        $tabs=[['Bulan']];
+        //menambahkan header data sesuai dengan tahun yang ditemukan
+        foreach($keys as $row)
+        {
+            array_push($tabs[0],(string)$row);
+        }
+        //membuat data bulan dalam satu tahun
+        $bulan=['Jan','Feb','Mar','Apr','Mei','Jun','Jul','Agu','Sep','Okt','Nop','Des'];
+        //memasukkan data penjualan bulanan kedalam tabulasi
+        for($i=1; $i<=12; $i++)
+        {
+            $dt=[$bulan[$i-1]];
+            foreach($keys as $row)
+            {
+                array_push($dt,(int)$result[$row][$i]);
+            }
+            array_push($tabs,$dt);
+        }
+        return json_encode($tabs);  
+    }
+}
---
+++ application/views/visin.php
@@ -0,0 +1,116 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Visin Google Charts</title>
+    <link rel="stylesheet" href="<?php echo base_url('vendor/uikit/css/'); ?>uikit.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>    
+    <script type="text/javascript">
+        // Mengambil API visualisasi.
+        google.charts.load('current', {'packages':['corechart']});
+        //mengambil data dari variabel PHP
+        var region=[];
+        region['dataStr'] = '<?php echo $region;?>';        
+        region['dataArray'] = JSON.parse(region['dataStr']);   
+        //menggambar grafik
+        google.charts.setOnLoadCallback(function(){
+            drawChart(region['dataArray'], 'pie','region');       
+        });
+
+        var sales=[];
+        sales['dataStr'] = '<?php echo $sales;?>';        
+        sales['dataArray'] = JSON.parse(sales['dataStr']);
+  
+        google.charts.setOnLoadCallback(function(){
+            drawChart(region['dataArray'], 'pie','region');
+            drawChart(sales['dataArray'],'bar','sales');        
+        });
+
+        var produk=[];
+        produk['dataStr'] = '<?php echo $produk;?>';        
+        produk['dataArray'] = JSON.parse(produk['dataStr']);    
+        //menggambar grafik
+        google.charts.setOnLoadCallback(function(){
+            drawChart(region['dataArray'], 'pie','region');
+            drawChart(sales['dataArray'],'bar','sales');
+            drawChart(produk['dataArray'],'bar','produk');        
+        });
+
+        var bulanan=[];
+        bulanan['dataStr'] = '<?php echo $bulanan;?>';        
+        bulanan['dataArray'] = JSON.parse(bulanan['dataStr']);
+        //menggambar grafik
+        google.charts.setOnLoadCallback(function(){
+            drawChart(region['dataArray'], 'pie','region');
+            drawChart(sales['dataArray'],'bar','sales');
+            drawChart(produk['dataArray'],'bar','produk');
+            drawChart(bulanan['dataArray'],'bar','bulanan');
+        });
+
+        // Menentukan data yang akan ditampilkan
+        function drawChart(dataArray,type,container) {
+            // Membuat data tabel yang sesuai dengan format google chart dari sumber data array javascript
+            var data = new google.visualization.arrayToDataTable(dataArray,false);      
+            // Tentukan pengaturan chart
+            var options = {
+                legend:'bottom',            
+                titlePosition:'none',
+                titleTextStyle:{fontSize:18},
+                chartArea:{width:'80%',height:'70%'}                      
+                };
+            if(type == 'pie')
+            {
+                var chart = new google.visualization.PieChart(document.getElementById(container));
+            }
+            if(type == 'column')
+            {
+                var chart = new google.visualization.ColumnChart(document.getElementById(container));
+            }
+            if(type == 'bar')
+            {
+                var chart = new google.visualization.BarChart(document.getElementById(container));
+            }
+            chart.draw(data, options);
+        }
+    </script> 
+</head>
+<body>
+    <nav class="uk-navbar-container uk-margin" uk-navbar>
+        <div class="uk-navbar-left">
+            <a class="uk-navbar-item uk-logo" href="#">Analisis Penjualan</a>
+        </div>
+    </nav>
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@s" uk-grid uk-height-match="target: > div > .uk-card">    
+            <div>
+                <div class="uk-card uk-card-default uk-card-small uk-card-body" >
+                    <h3 class="uk-card-title">Penjualan Berdasarkan Region</h3>
+                    <div id="region" style="height:350px;"></div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-small uk-card-body" >
+                    <h3 class="uk-card-title">Hasil Penjualan Oleh Sales</h3>
+                    <div id="sales" style="height:350px;"></div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-small uk-card-body" >
+                    <h3 class="uk-card-title">Produk Terlaris</h3>
+                    <div id="produk" style="height:350px;"></div>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-small uk-card-body" >
+                    <h3 class="uk-card-title">Penjualan Bulanan</h3>
+                    <div id="bulanan" style="height:350px;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="<?php echo base_url('vendor/uikit/js/'); ?>uikit.js"></script>
+</body>
+</html>
